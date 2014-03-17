3225202d2d07ff9d99d69ae70ee127b4b19d40c7
Joel Kemp <mrjoelkemp@gmail.com>
2014-03-17 16:57:16 -0400

    Added promisified exists

--- helpers/Utils.js
+++ helpers/Utils.js
@@ -1,6 +1,19 @@
 // Collection of little utility helpers
+var fs = require('fs'),
+    q = require('q');
 
 // Helper to return a slash-trailed version of the directory name
 module.exports.slashDir = function (directory) {
   return directory[directory.length - 1] === '/' ? directory : directory + '/';
+};
+
+// Promisified version of fs.exists
+module.exports.exists = function (path) {
+  var deferred = q.defer();
+
+  fs.exists(path, function (exists) {
+    deferred.resolve(exists);
+  });
+
+  return deferred.promise;
 };
